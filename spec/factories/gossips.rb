9856f7efb6ea7e926a42c6cda3f3718a19abb29d
Saroumoun <bastien.hiel@gmail.com>
2019-02-04 23:40:51 +0100

    added margin to view

--- spec/factories/gossips.rb
+++ spec/factories/gossips.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :gossip do
     title { Faker::Book.title }
     content { Faker::Hobbit.quote }
-    city {FactoryBot.create(:user)}
+    user {FactoryBot.create(:user)}
   end
 end
