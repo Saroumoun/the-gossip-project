dcc16f06bed6b2e0a329dbe810aac9edbb649735
Saroumoun <bastien.hiel@gmail.com>
2019-02-04 18:53:50 +0100

    added a lot of things

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
 	has_many :gossips
 	has_many :sent_messages, foreign_key: 'sender_id', class_name: "Message"
 	has_many :received_messages, foreign_key: 'recipient_id', class_name: "Message"
+
+	validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+
+  validates :first_name, presence:true
+
+  validates :last_name, presence:true
+
+  validates :description, presence:true
+
+  validates :age, presence:true
+
+  validates :city, presence:true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'gossip_controller#index'
+  get 'gossip/:id', to: 'gossip_controller#show_gossip'
+	get 'welcome/:first_name', to: 'dynamic_pages#welcome'
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
